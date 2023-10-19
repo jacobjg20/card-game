c24d7dbafa33f2d78802a0c280c9af39c74d1449
jacobjg20 <35909673+jacobjg20@users.noreply.github.com>
2023-10-19 08:25:43 +0000

    Created a way to transfer data from client to server

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,11 @@
 Working on game logic
 Creating a deck with 52 cards
 Working on populating values for deck
+
+Learn how fetch, and req and res work in a promise.
+how to access data 
+
+Fetch Data must be sent as a JSON string when being communicated over the interent. Then the browser and server must turn that JSON data into data we can read. 
+
+Make client display the cards that are currently in game
+
--- index.js
+++ index.js
@@ -1,32 +1,67 @@
+//Dependencies
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = 7070;
 const options = {
     root: path.join(__dirname)
 };
+//enables the app to use a tools from bodyParser
+app.use( bodyParser.json() ); //allows the reading of post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Instantiates and starts a copy of the game
 var Deck = require('./deck')
 var deck = new Deck();
 
-//Gives constant updates on the game
+//loop for apps inner tick rate
 function gameState(){
     setTimeout(function () {  
         gameState(); 
-        console.log(deck.cards);
+        console.log('running');
     }, 3000 );
 }
 gameState();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+//starts server
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", PORT);
 })
 
+//links to user pages
 app.get('/', function(req, res) {
 
     let fileName = '/index.html';
+
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            throw(err);
+        } else {
+            
+        }
+    });
+});
+
+app.get('/game', function(req, res) {
+    console.log('game has been sent');
+    res.send('game');
+});
+
+app.get('/about', function(req, res) {
+    res.send('about');
+});
+
+//links to assets
+app.get('/card-game.jpg', function(req, res) {
+
+    let fileName = '/card-game.jpg';
 
     res.sendFile(fileName, options, function (err) {
         if (err) {
@@ -50,9 +85,9 @@ app.get('/style.css', function(req, res) {
     });
 });
 
-app.get('/card-game.jpg', function(req, res) {
+app.get('/client-side-script.js', function(req, res) {
 
-    let fileName = '/card-game.jpg';
+    let fileName = '/client-side-script.js';
 
     res.sendFile(fileName, options, function (err) {
         if (err) {
@@ -63,13 +98,10 @@ app.get('/card-game.jpg', function(req, res) {
     });
 });
 
-app.get('/game', function(req, res) {
-    console.log('game has been sent');
-    res.send('game');
+//Post requests
+ app.post('/post-test', (req, res) => {
+    console.log('server has been posted');
+    console.log(req.body.name);
+    res.sendStatus(200);
 });
 
-app.get('/about', function(req, res) {
-    res.send('about');
-});
-
-//test code
